9308a06796f19bb3fc45ce92d5236601e5ca1ce8
NyaNyak <chsgood8182@naver.com>
2022-11-27 16:25:20 +0900

    플레이어 렌더링

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-const startX = 1024 / 2;
-const startY = 768 / 2;
+const startX = 800 / 2;
+const startY = 520 / 2;
 
 class Player {
   constructor(socket) {
@@ -65,37 +65,42 @@ endGame = (socket) => {
   delete playerMap[socket.id];
 };
 
+if (players.length < 2) {
+}
+
 io.on("connection", (socket) => {
-  console.log(`${socket.id}님 입장`);
-  socket.on("disconnect", (reason) => {
-    console.log(`${socket.id}님 ${reason}때문에 퇴장`);
-    endGame(socket);
-    socket.broadcast.emit("leave_user", socket.id);
-  });
+  if (players.length < 2) {
+    console.log(`${socket.id}님 입장`);
+    socket.on("disconnect", (reason) => {
+      console.log(`${socket.id}님 ${reason}때문에 퇴장`);
+      endGame(socket);
+      socket.broadcast.emit("leave_user", socket.id);
+    });
 
-  let newPlayer = joinGame(socket);
-  socket.emit("user_id", socket.id);
+    let newPlayer = joinGame(socket);
+    socket.emit("user_id", socket.id);
 
-  for (let i = 0; i < players.length; i++) {
-    let player = players[i];
-    socket.emit("join_user", {
-      id: player.id,
-      x: player.x,
-      y: player.y,
+    for (let i = 0; i < players.length; i++) {
+      let player = players[i];
+      socket.emit("join_user", {
+        id: player.id,
+        x: player.x,
+        y: player.y,
+      });
+    }
+
+    socket.broadcast.emit("join_user", {
+      id: socket.id,
+      x: newPlayer.x,
+      y: newPlayer.y,
+    });
+
+    socket.on("send_location", (data) => {
+      socket.broadcast.emit("update_state", {
+        id: data.id,
+        x: data.x,
+        y: data.y,
+      });
     });
   }
-
-  socket.broadcast.emit("join_user", {
-    id: socket.id,
-    x: newPlayer.x,
-    y: newPlayer.y,
-  });
-
-  socket.on("send_location", (data) => {
-    socket.broadcast.emit("update_state", {
-      id: data.id,
-      x: data.x,
-      y: data.y,
-    });
-  });
 });
