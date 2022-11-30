1be1da0ea9c0b414c2ae0767115bfb386de180ef
NyaNyak <chsgood8182@naver.com>
2022-11-30 23:32:08 +0900

    로비 음악 추가

--- views/gameLobby.js
+++ views/gameLobby.js
@@ -1,4 +1,14 @@
+const lobby = document.getElementById("lobbyBgm");
+
+window.onpageshow = function (event) {
+  lobby.load();
+  lobby.volume = "0.3";
+  lobby.loop = true;
+  lobby.play();
+};
+
 enterGame = () => {
+  lobby.pause();
   let nickname = document.getElementById("inputName").value;
   console.log(nickname);
 };
